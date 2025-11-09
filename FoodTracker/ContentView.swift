3c57f6438798b9754ea0db94dd9bf8e40361e571
sv_muromtsev <k29393@yandex.ru>
2025-11-09 17:30:59 +0500

    Initial Commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Sergei Muromtsev on 09.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!зззз")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodTracker/FoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackerApp.swift
+//  FoodTracker
+//
+//  Created by Sergei Muromtsev on 09.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
